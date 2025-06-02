3b7cedf38f59f20951884735e0b9285fa85e5bb8
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2025-06-02 22:39:45 +0300

    change: CMAKE_SOURCE_DIR -> CMAKE_CURRENT_SOURCE_DIR in cmake_paths.cmake

--- cmake_paths.cmake
+++ cmake_paths.cmake
@@ -1,11 +1,11 @@
 # language: CMake
 
-cmake_path(SET QCUSTOMPLOT_DIR "${CMAKE_SOURCE_DIR}/qcustomplot")
-cmake_path(SET CMAKESCRIPTS_DIR "${CMAKE_SOURCE_DIR}/cmake_scripts")
-cmake_path(SET IMAGES_DIR "${CMAKE_SOURCE_DIR}/.extra/images")
+cmake_path(SET QCUSTOMPLOT_DIR "${CMAKE_CURRENT_SOURCE_DIR}/qcustomplot")
+cmake_path(SET CMAKESCRIPTS_DIR "${CMAKE_CURRENT_SOURCE_DIR}/cmake_scripts")
+cmake_path(SET IMAGES_DIR "${CMAKE_CURRENT_SOURCE_DIR}/.extra/images")
 
-cmake_path(SET PROJECT_DIR "${CMAKE_SOURCE_DIR}")
+cmake_path(SET PROJECT_DIR "${CMAKE_CURRENT_SOURCE_DIR}")
 
-cmake_path(SET GUI_DIR "${CMAKE_SOURCE_DIR}/gui")
-cmake_path(SET DATATOOLS_DIR "${CMAKE_SOURCE_DIR}/data_tools")
-cmake_path(SET MATH_DIR "${CMAKE_SOURCE_DIR}/math")
+cmake_path(SET GUI_DIR "${CMAKE_CURRENT_SOURCE_DIR}/gui")
+cmake_path(SET DATATOOLS_DIR "${CMAKE_CURRENT_SOURCE_DIR}/data_tools")
+cmake_path(SET MATH_DIR "${CMAKE_CURRENT_SOURCE_DIR}/math")
